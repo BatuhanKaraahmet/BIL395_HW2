a8b54773c3d001f68a8adc2159fd9f94683dbc6c
BatuhanKaraahmet <135848588+BatuhanKaraahmet@users.noreply.github.com>
2025-03-28 23:11:03 +0300

    Update perl_calc.pl

--- perl_calc.pl
+++ perl_calc.pl
@@ -1,1 +1,50 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my %vars;
+
+print "> ";
+while (my $line = <STDIN>) {
+    chomp $line;
+    $line =~ s/^\s+|\s+$//g;
+    last if $line =~ /^\s*(exit|quit)\s*$/i;
+
+    if ($line =~ /^(\w+)\s*=\s*(.+)$/) {
+        my ($var, $expr) = ($1, $2);
+        my $eval_expr = substitute_vars($expr);
+        my $result = safe_eval($eval_expr);
+        if (defined $result) {
+            $vars{$var} = $result;
+            print "$result\n";
+        }
+    } else {
+        my $eval_expr = substitute_vars($line);
+        my $result = safe_eval($eval_expr);
+        print defined $result ? "$result\n" : "";
+    }
+    print "> ";
+}
+
+sub substitute_vars {
+    my ($expr) = @_;
+    $expr =~ s/(\b\w+\b)/exists $vars{$1} ? $vars{$1} : $1/eg;
+    return $expr;
+}
+
+sub safe_eval {
+    my ($expr) = @_;
+    my $result = eval "$expr";
+    if ($@) {
+        print "Error: Invalid expression\n";
+        return undef;
+    } elsif ($expr =~ /\/0(?!\d)/) {
+        print "Error: Division by zero\n";
+        return undef;
+    } elsif ($expr =~ /[a-zA-Z]/ && $expr =~ /\b(\w+)\b/ && !exists $vars{$1}) {
+        print "Error: Undefined variable '$1'\n";
+        return undef;
+    }
+    return $result;
+}
 
